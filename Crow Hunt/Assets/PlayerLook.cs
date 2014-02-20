b420ea1132fc4a9392d94c6d1c0ec2c098f4a2d2
Comeonyougit-WORK <jakedesu@gmail.com>
2014-02-20 16:25:37 +0400

    koroch normal
    hui

---
+++ Crow Hunt/Assets/PlayerLook.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLook : MonoBehaviour {
+	public Rigidbody2D stone;
+	public Transform crosshair;
+	public float speedModifier;
+
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+	}
+	
+	// Update is called once per frame 
+	void Update () {
+		crosshair.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, -6)) / 3;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Rigidbody2D projectile = (Rigidbody2D)Instantiate(stone, new Vector3(this.transform.position.x,this.transform.position.y), Quaternion.identity);
+			projectile.AddForce(new Vector2(crosshair.position.x - this.transform.position.x, crosshair.position.y - this.transform.position.y) * speedModifier);
+		}
+	}
+}
---
+++ Crow Hunt/Assets/StoneThrow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneThrow : MonoBehaviour {
+	public Transform crosshair;
+	public float speedModifier = 10;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddForce(new Vector2(crosshair.position.x * speedModifier, crosshair.position.y * speedModifier));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
